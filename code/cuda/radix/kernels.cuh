e988e090d0bc57f4818c8214ed9ab5ad28715d86
Christian Nielsen <christiannhn@hotmail.com>
2025-11-05 12:07:32 +0100

    removed more device syncs

--- code/cuda/radix/kernels.cuh
+++ code/cuda/radix/kernels.cuh
@@ -10,12 +10,10 @@ histogramKer( uint32_t* input
   // Shared memory buffer
   __shared__ uint32_t sh_hist[H];
 
-  // Zeroing shared histogram
+  // Zeroing shared histogram (only works for H >= B)
   for (int i = threadIdx.x; i < H; i += blockDim.x) {
     sh_hist[i] = 0;
   }
-  __syncthreads();
-
 
   //coalesced access on block level - should be warp level??
   uint32_t block_start = blockIdx.x * (blockDim.x * Q);
@@ -30,7 +28,7 @@ histogramKer( uint32_t* input
 
   __syncthreads();
 
-  // Copy shared histogram to global memory
+  // Copy shared histogram to global memory (only works for H >= B)
   for (int i = threadIdx.x; i < H; i += blockDim.x) {
       histogram[blockIdx.x * H + i] = sh_hist[i];
   }
@@ -59,12 +57,10 @@ coalsTransposeKer(uint32_t* A, uint32_t* C, int heightA, int widthA) {
       C[y*heightA + x] = tile[threadIdx.x][threadIdx.y];
 }
 
-
+// Creates a flag array representing the (static) per block histogram size
 __global__ void createFlagKer(char* d_out, const size_t N) {
   int tid = threadIdx.x + blockIdx.x * blockDim.x;
-  if (tid < N) {
-    d_out[tid] = tid % H == 0;
-  } 
+  if (tid < N) d_out[tid] = tid % H == 0;
 }
 
 // Partition and scatter kernel 
@@ -76,7 +72,7 @@ __global__ void scatterKer(uint32_t* input,
                            const size_t bits,
                            const size_t shift) {
 
-  // Local shared histogram across the block
+  // Local shared elements and histogram across the block
   __shared__ uint32_t elms[Q*B];
   __shared__ uint32_t elms_shared[Q*B];
   __shared__ uint32_t histogram_block[B];
@@ -93,7 +89,7 @@ __global__ void scatterKer(uint32_t* input,
     }
   }
 
-  // Sort elements in block
+  // Sort elements in block (same as performing partition2 on each bit starting with least sig.)
   for (int b = 0; b < bits; b++){
     uint32_t sum = 0;
 
@@ -111,23 +107,23 @@ __global__ void scatterKer(uint32_t* input,
     
     // Scan excl block    
     int offset = 1;
-    // Up sweep
+    // Up sweep in logB steps
     for (int d = B >> 1; d > 0; d >>= 1) {
         __syncthreads();
-        if (threadIdx.x < d)
+        if ( threadIdx.x < d)
             histogram_block_scan[offset * (2 * threadIdx.x + 2) - 1] += histogram_block_scan[offset * (2 * threadIdx.x + 1) - 1];
-        offset *= 2;
+        offset <<= 1;
     }
 
     // Clear the last element for exclusive scan
-    if (threadIdx.x == 0)
+    if ( threadIdx.x == 0)
         histogram_block_scan[B - 1] = 0;
 
-    // Down sweep
+    // Down sweep in logB steps
     for (int d = 1; d < B; d *= 2) {
         offset >>= 1;
         __syncthreads();
-        if (threadIdx.x < d) {
+        if ( threadIdx.x < d) {
             int ai = offset * (2 * threadIdx.x + 1) - 1;
             int bi = offset * (2 * threadIdx.x + 2) - 1;
             int t = histogram_block_scan[ai];
